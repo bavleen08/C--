81b58cad6494a3fb8490cb54a7d4e0f27383e000
Bavleen Kaur <you@example.com>
2025-07-21 17:18:21 +0530

    done

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {  // main function
+    // cout << "hello world"<<endl <<"hi there"; // std::cout << "hello"
+ // data types
+    // int age = 25;
+    // char grade = 'A';
+    // float PI = 3.14f;
+    // double price = 23.4;
+    // bool isSafe = true; // true-> 1 false-> 0
+
+ // type casting
+    // char grades = 'b'; // 98
+    // int value = grades; // implicit
+    
+    // double prices = 45.6; // even if it wouldne 45.9, the int value will still be 45
+    // int newPrice = (int)prices; // explicit
+
+// input
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age is: " << age  ;
+    return 0;
+}
