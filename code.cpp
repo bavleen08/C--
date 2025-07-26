c6fca3aeed945080efb6716db6dd5726bcc8f68f
Bavleen Kaur <you@example.com>
2025-07-26 22:19:52 +0530

    inerted triangle que

--- code.cpp
+++ code.cpp
@@ -206,7 +206,18 @@ int main() {  // main function
 //     cout << endl;
 // }
 
-
+int n = 4;
+for(int i=0; i<n; i++) {
+    //spaces
+    for(int j=0; j<i; j++) {
+        cout << " ";
+    }
+    //nums
+    for(int j=0; j<n-i; j++) {
+        cout << (i+1);
+    }
+    cout << endl;
+}
 }
 
 
