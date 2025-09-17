b3de44dfb25815971e249c0964bff6e4dbf85bfd
Bavleen Kaur <you@example.com>
2025-09-17 22:43:23 +0530

    kadane algorithm

--- DSA/subarray.cpp
+++ DSA/subarray.cpp
@@ -15,6 +15,7 @@ void printSubarrays(int *arr, int n){
     }
 }
 
+// BRUTE FORCE
 void maxSubarraySum1(int *arr, int n){
     int max_Sum = INT_MIN;
     for(int start = 0; start <n; start ++){
@@ -29,12 +30,41 @@ void maxSubarraySum1(int *arr, int n){
         cout<<endl;
     }
     cout << "Maximum sum is: "<< max_Sum;
-
 }
+
+// OPTIMIZATION
+void maxSubarraySum2(int *arr, int n){
+    int max_Sum = INT_MIN;
+    for(int start = 0; start <n; start ++){
+        int currsum = 0;
+        for(int end=start; end<n; end++){
+            currsum += arr[end];
+            cout << currsum <<", ";
+            max_Sum = max(max_Sum , currsum);
+        }
+        cout<<endl;
+    }
+    cout << "Maximum sum is: "<< max_Sum;
+}
+
+// KADANE'S ALGORITHM
+void maxSubarraySum3(int *arr, int n){
+    int max_Sum = INT_MIN;
+    int currsum = 0;
+    for(int start = 0; start <n; start ++){
+        currsum += arr[start];
+        max_Sum = max(max_Sum , currsum);
+        if (currsum < 0){
+            currsum = 0;
+        }
+    }
+    cout << "Maximum sum is: "<< max_Sum;
+}
+
 int main(){
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {2,-3, 6,-5, 4, 2};
     int n = sizeof(arr)/sizeof(int);
     //printSubarrays(arr , n);
-    maxSubarraySum1(arr, n);
+    maxSubarraySum3(arr, n);
 
 }
