87d2cc55cf9e9f12b84ff37b0b90126c1a51ec46
Bavleen Kaur <you@example.com>
2025-09-18 23:43:15 +0530

    buy and sell stocks

---
+++ DSA/Qbuysell.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void maxProfit(int *prices, int n){
+    int bestBuy[100000];
+    bestBuy[0] =INT_MAX;
+    for(int i=1; i<n; i++){   // TC = O(n)
+        bestBuy[i] = min(bestBuy[i-1], prices[i-1]);
+    }
+    int maxProfit = 0;
+    for(int i=0; i<n; i++){    // TC = O(n)
+        int currprofit = prices[i] - bestBuy[i];
+        maxProfit = max(currprofit, maxProfit);
+    }
+    cout << "maximum profit: "<< maxProfit<< endl;
+    // TC = O(n+n) = O(2n) = O(n)
+}
+int main(){
+    int prices[6] = {7,1,5,3,6,4};
+    int n = sizeof(prices) / sizeof(int);
+    maxProfit(prices, n);
+}
---
+++ DSA/Qtrapwater.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void trapRainwater(int *height, int n){
+    
+}
+
+int main(){
+    int height[7] = {4,2,0,6,3,2,5};
+    int n = sizeof(height) / sizeof(int);
+}
